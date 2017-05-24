374e6485a67a66587d261893b2b2f40c4f6fa4bd
prudywsh <antoineprudhomme5@gmail.com>
2017-05-24 14:52:02 +0200

    template done

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// to use /public in html files
+app.use('/public', express.static('public'));
+
+// default route
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
+// run the app
 app.listen(3000, function () {
   console.log('Listening on port 3000!');
 });
