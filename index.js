feaa05acf5cd2433ee4b6d3a35867e91c9c1478d
vrajau <vincent.rajau@utt.fr>
2017-06-12 02:19:43 +0200

    Aded configuration api + key

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
+const Twitter = require('twitter')
+const client = new Twitter({
+	consumer_key: process.env.CONSUMER_KEY,
+	consumer_secret: process.env.CONSUMER_KEY_SECRET,
+	access_token_key: process.env.TOKEN,
+	access_token_secret: process.env.TOKEN_SECRET
+});
 
 // to use /public in html files
 app.use('/public', express.static('public'));
